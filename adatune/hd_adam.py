Fixed the hyper-lr update computation
mu and hyper-lr update is turned off by default

adatune/hd_adam.py
@@ -75,8 +75,8 @@ class AdamHD(Optimizer):
                     prev_bias_correction1 = 1 - beta1 ** (state['step'] - 1)
                     prev_bias_correction2 = 1 - beta2 ** (state['step'] - 1)
                     # Hypergradient for Adam:
-                    h = torch.dot(grad.view(-1),
-                                  torch.div(exp_avg, exp_avg_sq.sqrt().add_(group['eps'])).view(-1)) * math.sqrt(
+                    h = torch.dot(grad.reshape(-1),
+                                  torch.div(exp_avg, exp_avg_sq.sqrt().add_(group['eps'])).reshape(-1)) * math.sqrt(
                         prev_bias_correction2) / prev_bias_correction1
                     # Hypergradient descent of the learning rate:
                     group['lr'] += group['hypergrad_lr'] * h

adatune/hd_sgd.py
@@ -48,11 +48,11 @@ class SGDHD(Optimizer):
             if p.grad is None:
                 view = torch.zeros_like(p.data)
             elif p.grad.data.is_sparse:
-                view = p.grad.data.to_dense().view(-1)
+                reshape = p.grad.data.to_dense().reshape(-1)
             else:
-                view = p.grad.data.view(-1)
+                reshape = p.grad.data.reshape(-1)
             if weight_decay != 0:
-                view.add_(weight_decay, p.data.view(-1))
+                reshape.add_(weight_decay, p.data.reshape(-1))
             views.append(view)
         return torch.cat(views, 0)
 

adatune/mu_adam.py
@@ -21,7 +21,8 @@ from adatune.utils import *
 
 class MuAdam(object):
 
-    def __init__(self, optimizer, hyper_lr, grad_clipping, first_order, mu, alpha, device):
+    def __init__(self, optimizer, hyper_lr, adapt_hyper_lr, grad_clipping, first_order, mu, adapt_mu, hyper_hyper_lr,
+                 device):
         self.optimizer = optimizer
         self.lr = self.optimizer.param_groups[0]['lr']
         self.beta1 = self.optimizer.param_groups[0]['betas'][0]
@@ -34,8 +35,9 @@ class MuAdam(object):
         self.first_order = first_order
         self.device = device
         self.mu = mu
-        self.mu_mode = 'auto' if self.mu < 0.0 else 'manual'
-        self.alpha = alpha
+        self.adapt_mu = adapt_mu
+        self.adapt_hyper_lr = adapt_hyper_lr
+        self.hyper_hyper_lr = hyper_hyper_lr
         self.z_0 = None
         self.z_1 = None
         self.z_2 = None
@@ -43,10 +45,11 @@ class MuAdam(object):
         self.b = None
         self.c = 0.0
         self.state_init = False
+        self.past_hyper_grad = None
 
     def flatten_state(self, net):
-        return (torch.cat([self.optimizer.state[v]['exp_avg'].view(-1) for v in net.parameters()]),
-                torch.cat([self.optimizer.state[v]['exp_avg_sq'].view(-1) for v in net.parameters()]))
+        return (torch.cat([self.optimizer.state[v]['exp_avg'].reshape(-1) for v in net.parameters()]),
+                torch.cat([self.optimizer.state[v]['exp_avg_sq'].reshape(-1) for v in net.parameters()]))
 
     def clip_grad(self, net):
         if self.grad_clipping:
@@ -61,7 +64,7 @@ class MuAdam(object):
             return
 
         self.clip_grad(first_grad)
-        grad_flatten = torch.cat([g.view(-1) for g in first_grad]).requires_grad_(True)
+        grad_flatten = torch.cat([g.reshape(-1) for g in first_grad]).requires_grad_(True)
 
         coeff = (math.sqrt(1.0 - self.beta2 ** self.step)) / (1.0 - self.beta1 ** self.step)
 
@@ -74,7 +77,7 @@ class MuAdam(object):
             hvp = ag.grad(grad_flatten @ self.z_2, net.parameters())
             self.clip_grad(hvp)
             grad_flatten = grad_flatten.detach()
-            hvp_flatten = torch.cat([h.view(-1) for h in hvp])
+            hvp_flatten = torch.cat([h.reshape(-1) for h in hvp])
 
             m_t, v_t = self.flatten_state(net)
 
@@ -85,7 +88,7 @@ class MuAdam(object):
             a_33 = (1.0 - self.lr * coeff) * (a_33_inner_1 - a_33_inner_2) * hvp_flatten
 
             self.z_2 = self.mu * (a_31 * self.z_0 + a_32 * self.z_1 + a_33)
-            self.z_2 = self.z_2 + torch.neg(coeff * (m_t / torch.sqrt(v_t + self.eps)))
+            self.z_2 += torch.neg(coeff * (m_t / torch.sqrt(v_t + self.eps)))
 
             self.z_0 = self.mu * (self.beta1 * self.z_0 + (1.0 - self.beta1) * hvp_flatten)
             self.z_1 = self.mu * (self.beta2 * self.z_1 + 2.0 * (1.0 - self.beta2) * grad_flatten * hvp_flatten)
@@ -103,7 +106,7 @@ class MuAdam(object):
             return
 
         self.clip_grad(val_grad)
-        val_grad_flatten = torch.cat([f.view(-1) for f in val_grad])
+        val_grad_flatten = torch.cat([f.reshape(-1) for f in val_grad])
 
         mat_mul = val_grad_flatten @ self.z_2
         hyper_grad = mat_mul.item()
@@ -116,12 +119,17 @@ class MuAdam(object):
         for param_group in self.optimizer.param_groups:
             param_group['lr'] = new_lr
 
-        # # Update hyper-LR
-        for param_group in self.hyper_optim.param_groups:
-            param_group['lr'] = np.max([param_group['lr'] + self.alpha * hyper_grad * new_lr, 0.0])
+        # Update hyper-LR
+        if self.adapt_hyper_lr:
+            if self.past_hyper_grad:
+                for param_group in self.hyper_optim.param_groups:
+                    cur_hyper_lr = param_group['lr']
+                    new_hyper_lr = cur_hyper_lr + self.hyper_hyper_lr * (hyper_grad * self.past_hyper_grad)
+                    param_group['lr'] = new_hyper_lr
+            self.past_hyper_grad = hyper_grad
 
         # Update mu
-        if self.mu_mode == 'auto':
+        if self.adapt_mu:
             grad_mult = (val_grad_flatten @ self.b).item()
             q_norm = new_lr / grad_mult
             z = np.maximum(np.minimum(q_norm, 1.), 0.)

adatune/mu_sgd.py
@@ -19,7 +19,8 @@ from adatune.utils import *
 
 class MuSGD(object):
 
-    def __init__(self, optimizer, hyper_lr, grad_clipping, first_order, mu, alpha, device):
+    def __init__(self, optimizer, hyper_lr, adapt_hyper_lr, grad_clipping, first_order, mu, adapt_mu, hyper_hyper_lr,
+                 device):
         self.optimizer = optimizer
         self.lr = self.optimizer.param_groups[0]['lr']
         self.momentum = self.optimizer.param_groups[0]['momentum']
@@ -32,14 +33,16 @@ class MuSGD(object):
         self.z_0 = None
         self.z_1 = None
         self.c = 0.0
-        self.alpha = alpha
+        self.hyper_hyper_lr = hyper_hyper_lr
         self.mu = mu
-        self.mu_mode = 'auto' if self.mu < 0.0 else 'manual'
+        self.adapt_mu = adapt_mu
+        self.adapt_hyper_lr = adapt_hyper_lr
         self.b = None
         self.state_init = False
+        self.past_hyper_grad = None
 
     def flatten_state(self, net):
-        return torch.cat([self.optimizer.state[v]['momentum_buffer'].view(-1) for v in net.parameters()])
+        return torch.cat([self.optimizer.state[v]['momentum_buffer'].reshape(-1) for v in net.parameters()])
 
     def clip_grad(self, net):
         if self.grad_clipping:
@@ -54,7 +57,7 @@ class MuSGD(object):
 
         self.clip_grad(first_grad)
 
-        grad_flatten = torch.cat([g.view(-1) for g in first_grad]).requires_grad_(True)
+        grad_flatten = torch.cat([g.reshape(-1) for g in first_grad]).requires_grad_(True)
 
         if self.first_order or self.z_0 is None:
             self.z_0 = torch.neg(grad_flatten)
@@ -65,15 +68,15 @@ class MuSGD(object):
         else:
             hvp = ag.grad(grad_flatten @ self.z_0, net.parameters())
             self.clip_grad(hvp)
-            hvp_flatten = torch.cat([h.view(-1) for h in hvp])
+            hvp_flatten = torch.cat([h.reshape(-1) for h in hvp])
             if self.momentum > 0.0:
                 v_t = self.flatten_state(net)
                 self.z_0 = self.mu * (self.z_0 - (self.lr * hvp_flatten + self.lr * self.momentum * self.z_1))
-                self.z_0 = self.z_0 + torch.neg(self.momentum * v_t + grad_flatten)
+                self.z_0 += torch.neg(self.momentum * v_t + grad_flatten)
                 self.z_1 = self.mu * (hvp_flatten + self.momentum * self.z_1)
             else:
                 self.z_0 = self.mu * (self.z_0 - self.lr * hvp_flatten)
-                self.z_0 = self.z_0 + torch.neg(grad_flatten)
+                self.z_0 += torch.neg(grad_flatten)
 
         self.z_0 = self.z_0.detach()
         if self.momentum > 0.0:
@@ -87,7 +90,7 @@ class MuSGD(object):
             return
 
         self.clip_grad(val_grad)
-        val_grad_flatten = torch.cat([f.view(-1) for f in val_grad])
+        val_grad_flatten = torch.cat([f.reshape(-1) for f in val_grad])
 
         mat_mul = val_grad_flatten @ self.z_0
         hyper_grad = mat_mul.item()
@@ -101,11 +104,16 @@ class MuSGD(object):
             param_group['lr'] = new_lr
 
         # Update hyper-LR
-        for param_group in self.hyper_optim.param_groups:
-            param_group['lr'] = np.max([param_group['lr'] + self.alpha * hyper_grad * new_lr, 0.0])
+        if self.adapt_hyper_lr:
+            if self.past_hyper_grad:
+                for param_group in self.hyper_optim.param_groups:
+                    cur_hyper_lr = param_group['lr']
+                    new_hyper_lr = cur_hyper_lr + self.hyper_hyper_lr * (hyper_grad * self.past_hyper_grad)
+                    param_group['lr'] = new_hyper_lr
+            self.past_hyper_grad = hyper_grad
 
         # Update mu
-        if self.mu_mode == 'auto':
+        if self.adapt_mu:
             grad_mult = (val_grad_flatten @ self.b).item()
             q_norm = new_lr / grad_mult
             z = np.maximum(np.minimum(q_norm, 1.), 0.)

adatune/network.py
@@ -134,7 +134,7 @@ class ResNet(nn.Module):
         out = self.layer3(out)
         out = self.layer4(out)
         out = F.avg_pool2d(out, 4)
-        out = out.view(out.size(0), -1)
+        out = out.reshape(out.size(0), -1)
         out = self.linear(out)
         return out
 
@@ -229,7 +229,7 @@ class WideResNet(nn.Module):
         out = self.layer3(out)
         out = F.relu(self.bn1(out))
         out = F.avg_pool2d(out, 8)
-        out = out.view(out.size(0), -1)
+        out = out.reshape(out.size(0), -1)
         out = self.linear(out)
 
         return out
@@ -265,7 +265,7 @@ class LeNet(nn.Module):
         out = F.max_pool2d(out, 2)
         out = F.relu(self.conv2(out))
         out = F.max_pool2d(out, 2)
-        out = out.view(out.size(0), -1)
+        out = out.reshape(out.size(0), -1)
         out = F.relu(self.fc1(out))
         out = F.relu(self.fc2(out))
         out = self.fc3(out)
@@ -285,7 +285,7 @@ class MLP(nn.Module):
         self.fc3 = nn.Linear(n_2, num_classes)
 
     def forward(self, din):
-        din = din.view(-1, 28 * 28)
+        din = din.reshape(-1, 28 * 28)
         dout = F.relu(self.fc1(din))
         dout = F.relu(self.fc2(dout))
         return self.fc3(dout)
@@ -394,7 +394,7 @@ class DenseNet(nn.Module):
         out = self.trans2(self.dense2(out))
         out = self.dense3(out)
         out = F.avg_pool2d(F.relu(self.bn1(out)), 8)
-        out = out.view(out.size(0), -1)
+        out = out.reshape(out.size(0), -1)
         out = self.fc(out)
         return out
 
@@ -421,7 +421,7 @@ class VGG(nn.Module):
 
     def forward(self, x):
         out = self.features(x)
-        out = out.view(out.size(0), -1)
+        out = out.reshape(out.size(0), -1)
         #out = self.dropout(out)
         out = self.classifier(out)
         return out

bin/rtho.py
@@ -35,18 +35,20 @@ def cli_def():
     parser.add_argument('--lr', type=float, default=0.01)
     parser.add_argument('--momentum', type=float, default=0.9)
     parser.add_argument('--wd', type=float, default=5e-4)
+    parser.add_argument('--adapt-hyper-lr', action='store_true')
     parser.add_argument('--hyper-lr', type=float, default=1e-8)
-    parser.add_argument('--alpha', type=float, default=1e-6)
+    parser.add_argument('--hyper-hyper-lr', type=float, default=1e-6)
     parser.add_argument('--model-loc', type=str, default='./model.pt')
     parser.add_argument('--grad-clipping', type=float, default=100.0)
+    parser.add_argument('--adapt-mu', action='store_true')
     parser.add_argument('--mu', type=float, default=0.99999)
     parser.add_argument('--first-order', action='store_true')
     parser.add_argument('--seed', type=int, default=42)
     return parser
 
 
-def train_rtho(network_name, dataset, num_epoch, batch_size, optim_name, lr, momentum, wd, hyper_lr, alpha, model_loc,
-               grad_clipping, first_order, seed, mu=1.0):
+def train_rtho(network_name, dataset, num_epoch, batch_size, optim_name, lr, momentum, wd, adapt_hyper_lr, hyper_lr,
+               hyper_hyper_lr, model_loc, grad_clipping, first_order, seed, adapt_mu, mu):
     torch.manual_seed(seed)
 
     # We are using cuda for training - no point trying out on CPU for ResNet
@@ -65,10 +67,12 @@ def train_rtho(network_name, dataset, num_epoch, batch_size, optim_name, lr, mom
 
     if optim_name == 'adam':
         optimizer = optim.Adam(net.parameters(), lr=lr, weight_decay=wd, eps=1e-4)
-        hyper_optim = MuAdam(optimizer, hyper_lr, grad_clipping, first_order, mu, alpha, device)
+        hyper_optim = MuAdam(optimizer, hyper_lr, adapt_hyper_lr, grad_clipping, first_order, mu, adapt_mu,
+                             hyper_hyper_lr, device)
     else:
         optimizer = optim.SGD(net.parameters(), lr=lr, momentum=momentum, weight_decay=wd)
-        hyper_optim = MuSGD(optimizer, hyper_lr, grad_clipping, first_order, mu, alpha, device)
+        hyper_optim = MuSGD(optimizer, hyper_lr, adapt_hyper_lr, grad_clipping, first_order, mu, adapt_mu,
+                            hyper_hyper_lr, device)
 
     vg = ValidationGradient(test_data, nn.CrossEntropyLoss(), device)
     for epoch in range(num_epoch):
@@ -123,5 +127,5 @@ if __name__ == '__main__':
         os.remove(args.model_loc)
 
     train_rtho(args.network, args.dataset, args.num_epoch, args.batch_size, args.optimizer, args.lr, args.momentum,
-               args.wd, args.hyper_lr, args.alpha, args.model_loc, args.grad_clipping, args.first_order, args.seed,
-               args.mu)
+               args.wd, args.adapt_hyper_lr, args.hyper_lr, args.hyper_hyper_lr, args.model_loc, args.grad_clipping,
+               args.first_order, args.seed, args.adapt_mu, args.mu)